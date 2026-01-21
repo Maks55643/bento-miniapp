6ef0c5386e5347ceb5e02fa62f2c7e607088e249
BENTO TEAM <143133560+Maks55643@users.noreply.github.com>
2026-01-22 01:43:25 +0300

    Update api.js

--- src/core/api.js
+++ src/core/api.js
@@ -1,1 +1,18 @@
+const API_BASE = "https://bentoapp-production.up.railway.app";
 
+export async function api(path, options = {}){
+  const res = await fetch(`${API_BASE}${path}`, {
+    headers: {
+      "Content-Type": "application/json",
+      ...options.headers
+    },
+    ...options
+  });
+
+  if (!res.ok) {
+    const err = await res.json().catch(() => ({}));
+    throw { status: res.status, ...err };
+  }
+
+  return res.json();
+}
