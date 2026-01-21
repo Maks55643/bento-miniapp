7171c496fbab3cbc6a193d509b99379e9f706ce3
BENTO TEAM <143133560+Maks55643@users.noreply.github.com>
2026-01-22 01:44:51 +0300

    Update loader.js

--- src/ui/loader.js
+++ src/ui/loader.js
@@ -1,1 +1,8 @@
+// ui/loader.js
+export function showLoader(){
+  document.getElementById("loading").style.display = "flex";
+}
 
+export function hideLoader(){
+  document.getElementById("loading").style.display = "none";
+}
