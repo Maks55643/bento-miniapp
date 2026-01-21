2ebe21ef3da36e5d1af0546d9bac791e7ce6a371
BENTO TEAM <143133560+Maks55643@users.noreply.github.com>
2026-01-22 01:42:54 +0300

    Update telegram.js

--- src/core/telegram.js
+++ src/core/telegram.js
@@ -1,1 +1,15 @@
+export let tg = null;
 
+export function initTelegram(){
+  if (!window.Telegram?.WebApp) {
+    throw new Error("Telegram WebApp not found");
+  }
+
+  tg = window.Telegram.WebApp;
+
+  tg.ready();
+  tg.expand();
+  tg.disableVerticalSwipes();
+  tg.setHeaderColor("#0e0f14");
+  tg.setBackgroundColor("#0e0f14");
+}
