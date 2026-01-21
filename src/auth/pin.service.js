e355ffe5adc78df21a41f7a22a5c702c8c9ef5b7
BENTO TEAM <143133560+Maks55643@users.noreply.github.com>
2026-01-22 01:44:16 +0300

    Update pin.service.js

--- src/auth/pin.service.js
+++ src/auth/pin.service.js
@@ -1,1 +1,16 @@
+import { state } from "../core/state.js";
+import { hashPin } from "../utils/hash.js";
+import { welcome } from "../ui/welcome.js";
 
+export async function checkPin(input){
+  const ok = await hashPin(input) === state.pinHash;
+
+  if (ok) {
+    state.attempts = 0;
+    welcome();
+    return true;
+  }
+
+  state.attempts++;
+  return false;
+}
